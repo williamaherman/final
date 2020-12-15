7014c2ffbbdb48742f2810f78a82399f4443ebda
William <bherman2424@gmail.com>
2020-12-14 22:33:30 -0500

    initial commit

--- db/init.sql
+++ db/init.sql
@@ -8,13 +8,12 @@ CREATE TABLE IF NOT EXISTS Inventory (
     `made_in` VARCHAR(32) CHARACTER SET utf8,
     `price` VARCHAR(8) CHARACTER SET utf8,
     `color` VARCHAR(32) CHARACTER SET utf8,
-    `condition` VARCHAR(32) CHARACTER SET utf8,
     PRIMARY KEY (`id`)
 );
-INSERT INTO Inventory(product_name, product_description, made_in, price, color, 'condition') VALUES
-    ('Nightstand', 'night stand built by prominent artist', 'New England', '125', 'white' 'fair'),
-    ('Coffee table', 'coffee table perfect for living room', 'China', '25', 'brown', 'good'),
-    ('Bed', 'queen sized bed', 'Brazil', '300', 'brown', 'excellent'),
-    ('lever', 'think about all the levers you''ved pulled', 'Unknown', '10', 'black', 'great'),
-    ('kitchen table', 'can sit 4-6 people', 'New Jersey', '100', 'yellow', 'fair'),
-    ('recliner', 'adjustable head rest and massage chair', 'Japan', '399', 'green', 'bad')
+INSERT INTO Inventory(product_name, product_description, made_in, price, color) VALUES
+    ('Nightstand', 'night stand built by prominent artist', 'New England', '125', 'white'),
+    ('Coffee table', 'coffee table perfect for living room', 'China', '25', 'brown'),
+    ('Bed', 'queen sized bed', 'Brazil', '300', 'brown'),
+    ('lever', 'think about all the levers you''ved pulled', 'Unknown', '10', 'black'),
+    ('kitchen table', 'can sit 4-6 people', 'New Jersey', '100', 'yellow'),
+    ('recliner', 'adjustable head rest and massage chair', 'Japan', '399', 'green')
