062a159929b4de9219617299cded25967b0547b3
William <bherman2424@gmail.com>
2020-12-16 23:07:34 -0500

    fixed html data to new csv

--- app/app.py
+++ app/app.py
@@ -18,49 +18,49 @@ def taxables():
     cursor = mysql.get_db().cursor()
     cursor.execute('SELECT * FROM taxables')
     result = cursor.fetchall()
-    return render_template('taxables.html', user=user, inventory=result)
+    return render_template('taxables.html', user=user, taxables=result)
 
 
-@app.route('/view/<int:taxables_id>', methods=['GET'])
-def show_taxables(taxables_id):
+@app.route('/view/<int:taxable_id>', methods=['GET'])
+def show_taxable(taxable_id):
     user = {'username': 'William'}
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM taxables WHERE id=%s', inventory_id)
+    cursor.execute('SELECT * FROM taxables WHERE id=%s', taxable_id)
     result = cursor.fetchall()
-    return render_template('view.html', user=user, inventory=result[0])
+    return render_template('view.html', user=user, taxable=result[0])
 
-@app.route('/edit/<int:taxables_id>', methods=['GET'])
-def edit_taxables(taxables_id):
+@app.route('/edit/<int:taxable_id>', methods=['GET'])
+def edit_taxable(taxable_id):
     user = {'username': 'William'}
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM taxables WHERE id=%s', inventory_id)
+    cursor.execute('SELECT * FROM taxables WHERE id=%s', taxable_id)
     result = cursor.fetchall()
-    return render_template('edit.html', user=user, inventory=result[0])
+    return render_template('edit.html', user=user, taxable=result[0])
 
 
-@app.route('/edit/<int:taxables_id>', methods=['POST'])
-def update_taxables(taxables_id):
+@app.route('/edit/<int:taxable_id>', methods=['POST'])
+def update_taxable(taxable_id):
     cursor = mysql.get_db().cursor()
-    sql = "UPDATE taxables SET Index= %s, Item=%s, Cost=%s, price=%s, Tax=%s, Total=%s WHERE id = %s"
+    sql = "UPDATE taxables SET Index= %s, Item=%s, Cost=%s, Tax=%s, Total=%s WHERE id = %s"
     req = request.form
     val = (
         req.get('Index'), req.get('Item'), req.get('Cost'), req.get('Tax'), req.get('Total'),
-        taxables_id)
+        taxable_id)
     cursor.execute(sql, val)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
 
 @app.route('/new', methods=['GET'])
-def new_taxables_form():
+def new_taxable_form():
     user = {'username': 'William'}
     return render_template('new.html', user=user)
 
 
 @app.route('/new', methods=['POST'])
-def create_taxables():
+def create_taxable():
     cursor = mysql.get_db().cursor()
-    sql = "INSERT INTO taxables (Index, Item, Cost, price, Tax, Total) VALUES (%s,%s,%s,%s,%s)"
+    sql = "INSERT INTO taxables (Index, Item, Tax, Total) VALUES (%s,%s,%s,%s,%s)"
     req = request.form
     val = (req.get('Index'), req.get('Item'), req.get('Cost'), req.get('Tax'), req.get('Total'))
     cursor.execute(sql, val)
@@ -68,18 +68,18 @@ def create_taxables():
     return redirect("/", code=302)
 
 
-@app.route('/delete/<int:taxables_id>', methods=['POST'])
-def delete_taxables(taxables_id):
+@app.route('/delete/<int:taxable_id>', methods=['POST'])
+def delete_taxable(taxable_id):
     cursor = mysql.get_db().cursor()
     sql = "DELETE FROM taxables WHERE id = %s"
-    val = (taxables_id,)
+    val = (taxable_id,)
     cursor.execute(sql, val)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
 
 @app.route('/api/v1/taxables', methods=['GET'])
-def list_taxables():
+def list_taxable():
     cursor = mysql.get_db().cursor()
     cursor.execute('SELECT * FROM taxables')
     result = cursor.fetchall()
@@ -102,7 +102,7 @@ def view_taxables(taxables_id):
 def change_taxables(taxables_id):
     cursor = mysql.get_db().cursor()
     content = request.json
-    sql = "UPDATE taxables SET Index= %s, Item=%s, Cost=%s, price=%s, Tax=%s, Total=%s WHERE id = %s"
+    sql = "UPDATE taxables SET Index= %s, Item=%s, Cost=%s, Tax=%s, Total=%s WHERE id = %s"
     val = (content['Index'], content['Item'], content['Cost'], content['Tax'], content['Total'])
     cursor.execute(sql, val)
     mysql.get_db().commit()
@@ -114,8 +114,8 @@ def change_taxables(taxables_id):
 def new_taxables():
     cursor = mysql.get_db().cursor()
     content = request.json
-    sql = "INSERT INTO taxables (product_name, product_description, made_in, price, color) VALUES (%s,%s,%s,%s,%s)"
-    val = (content['product_name'], content['product_description'], content['made_in'], content['cost'], content['color'])
+    sql = "INSERT INTO taxables (Index, Item, Cost, Tax, Total) VALUES (%s,%s,%s,%s,%s)"
+    val = (content['Index'], content['Item'], content['Cost'], content['Tax'], content['Total'])
     cursor.execute(sql, val)
     mysql.get_db().commit()
     resp = Response(status=200, mimetype='application/json')
@@ -126,7 +126,7 @@ def new_taxables():
 def remove_taxables(taxables_id):
     cursor = mysql.get_db().cursor()
     sql = "DELETE FROM taxables WHERE id = %s"
-    val = (inventory_id,)
+    val = (taxables_id,)
     cursor.execute(sql, val)
     mysql.get_db().commit()
     resp = Response(status=200, mimetype='application/json')
--- db/init.sql
+++ db/init.sql
@@ -2,21 +2,23 @@ CREATE DATABASE taxablesData;
 use taxablesData;
 
 CREATE TABLE IF NOT EXISTS taxables (
-    `Index` INT,
+    'id' int AUTO_INCREMENT,
+    'Index' int,
     `Item` VARCHAR(34) CHARACTER SET utf8,
     `Cost` VARCHAR(24) CHARACTER SET utf8,
     `Tax` NUMERIC(4, 2),
     `Total` NUMERIC(4, 2),
-    `Column_6` NUMERIC(4, 2)
+    `Column_6` NUMERIC(4, 2),
+    PRIMARY KEY (`id`)
 );
-INSERT INTO taxables VALUES
-    ( 1,' "Fruit of the Loom Girl''s Socks"','  7.97', 0.60,  8.57,NULL),
-    ( 2,' "Rawlings Little League Baseball"',' 2.97', 0.22,  3.19,NULL),
-    ( 3,' "Secret Antiperspirant"','           1.29', 0.10,  1.39,NULL),
-    ( 4,' "Deadpool DVD"','                   14.96', 1.12, 16.08,NULL),
-    ( 5,' "Maxwell House Coffee 28 oz"','      7.28', 0.55,  7.83,NULL),
-    ( 6,' "Banana Boat Sunscreen',' 8 oz"',     6.68, 0.50,  7.18),
-    ( 7,' "Wrench Set',' 18 pieces"',          10.00, 0.75, 10.75),
-    ( 8,' "M and M',' 42 oz"',                  8.98, 0.67,  9.65),
-    ( 9,' "Bertoli Alfredo Sauce"','           2.12', 0.16,  2.28,NULL),
-    (10,' "Large Paperclips',' 10 boxes"',      6.19, 0.46,  6.65);
+INSERT INTO taxables ('Index', Item, Cost, Tax, Total) VALUES
+    (100-150, 'Fruit of the Loom Girl''s Socks','$7.97', 0.60,  8.57,),
+    (100-175, 'Rawlings Little League Baseball', '$2.97', 0.22,  3.19,),
+    (200-225, 'Secret Antiperspirant', '$1.29', 0.10,  1.39),
+    (100-655, 'Deadpool DVD"', '14.96', '$1.12', 16.08),
+    (200-855, 'Maxwell House Coffee 28 oz', '$7.28', 0.55,  7.83),
+    (100-063, 'Banana Boat Sunscreen', '$6.68', 0.50,  7.18),
+    (100-352, 'Wrench Set', '$10.00', 0.75, 10.75),
+    (200-251, 'M and M', '$8.98', 0.67, 9.65),
+    (100-144, 'Bertoli Alfredo Sauce', '$2.12', 0.16,  2.28),
+    (200-150, 'Large Paperclips', '$6.19', 0.46,  6.65);
