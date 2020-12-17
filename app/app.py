d60ad14687492ecd608d78306fc93bf778b1a08b
William <bherman2424@gmail.com>
2020-12-16 22:03:54 -0500

    fixed sql data to the .csv

--- app/app.py
+++ app/app.py
@@ -9,100 +9,112 @@ app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'InventoryData'
+app.config['MYSQL_DATABASE_DB'] = 'taxablesData'
 mysql.init_app(app)
 
 @app.route('/', methods=['GET'])
-def inventory():
+def taxables():
     user = {'username': 'William'}
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM Inventory')
+    cursor.execute('SELECT * FROM taxables')
     result = cursor.fetchall()
-    return render_template('inventory.html', user=user, inventory=result)
+    return render_template('taxables.html', user=user, inventory=result)
 
 
-@app.route('/view/<int:inventory_id>', methods=['GET'])
-def show_inventory(inventory_id):
+@app.route('/view/<int:taxables_id>', methods=['GET'])
+def show_taxables(taxables_id):
     user = {'username': 'William'}
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM Inventory WHERE id=%s', inventory_id)
+    cursor.execute('SELECT * FROM taxables WHERE id=%s', inventory_id)
     result = cursor.fetchall()
     return render_template('view.html', user=user, inventory=result[0])
 
-@app.route('/edit/<int:inventory_id>', methods=['GET'])
-def edit_inventory(inventory_id):
+@app.route('/edit/<int:taxables_id>', methods=['GET'])
+def edit_taxables(taxables_id):
     user = {'username': 'William'}
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM Inventory WHERE id=%s', inventory_id)
+    cursor.execute('SELECT * FROM taxables WHERE id=%s', inventory_id)
     result = cursor.fetchall()
     return render_template('edit.html', user=user, inventory=result[0])
 
 
-@app.route('/edit/<int:inventory_id>', methods=['POST'])
-def update_inventory(inventory_id):
+@app.route('/edit/<int:taxables_id>', methods=['POST'])
+def update_taxables(taxables_id):
     cursor = mysql.get_db().cursor()
-    sql = "UPDATE Inventory SET product_name = %s, product_description=%s, made_in=%s, price=%s, color=%s WHERE id = %s"
+    sql = "UPDATE taxables SET Index= %s, Item=%s, Cost=%s, price=%s, Tax=%s, Total=%s WHERE id = %s"
     req = request.form
     val = (
-        req.get('product_name'), req.get('product_description'), req.get('made_in'), req.get('price'), req.get('color'),
-        inventory_id)
+        req.get('Index'), req.get('Item'), req.get('Cost'), req.get('price'), req.get('Tax'), req.get('Total'),
+        taxables_id)
     cursor.execute(sql, val)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
 
 @app.route('/new', methods=['GET'])
-def new_inventory_form():
+def new_taxables_form():
     user = {'username': 'William'}
     return render_template('new.html', user=user)
 
 
 @app.route('/new', methods=['POST'])
-def create_inventory():
+def create_taxables():
     cursor = mysql.get_db().cursor()
-    sql = "INSERT INTO Inventory (product_name, product_description, made_in, price, color) VALUES (%s,%s,%s,%s,%s)"
+    sql = "INSERT INTO taxables (Index, Item, Cost, price, Tax, Total) VALUES (%s,%s,%s,%s,%s)"
     req = request.form
-    val = (req.get('product_name'), req.get('product_description'), req.get('made_in'), req.get('price'), req.get('color'))
+    val = (req.get('Index'), req.get('Item'), req.get('Cost'), req.get('price'), req.get('Tax'), req.get('Total'))
     cursor.execute(sql, val)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
 
-@app.route('/delete/<int:inventory_id>', methods=['POST'])
-def delete_inventory(inventory_id):
+@app.route('/delete/<int:taxables_id>', methods=['POST'])
+def delete_taxables(taxables_id):
     cursor = mysql.get_db().cursor()
-    sql = "DELETE FROM Inventory WHERE id = %s"
-    val = (inventory_id,)
+    sql = "DELETE FROM taxables WHERE id = %s"
+    val = (taxables_id,)
     cursor.execute(sql, val)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
 
-@app.route('/api/v1/inventory', methods=['GET'])
-def list_inventory():
+@app.route('/api/v1/taxables', methods=['GET'])
+def list_taxables():
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM Inventory')
+    cursor.execute('SELECT * FROM taxables')
     result = cursor.fetchall()
     json_result = json.dumps(result)
     resp = Response(json_result, status=200, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/inventory/<int:inventory_id>', methods=['GET'])
-def view_inventory(inventory_id):
+@app.route('/api/v1/taxables/<int:taxables_id>', methods=['GET'])
+def view_taxables(taxables_id):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM Inventory WHERE id=%s', inventory_id)
+    cursor.execute('SELECT * FROM taxables WHERE id=%s', taxables_id)
     result = cursor.fetchall()
     json_result = json.dumps(result)
     resp = Response(json_result, status=200, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/inventory/<int:inventory_id>', methods=['PUT'])
-def change_inventory(inventory_id):
+@app.route('/api/v1/taxables/<int:taxables_id>', methods=['PUT'])
+def change_taxables(taxables_id):
     cursor = mysql.get_db().cursor()
     content = request.json
-    sql = "UPDATE Inventory product_name = %s, product_description=%s, made_in=%s, price=%s, color=%s WHERE id = %s"
+    sql = "UPDATE taxables SET Index= %s, Item=%s, Cost=%s, price=%s, Tax=%s, Total=%s WHERE id = %s"
+    val = (content['Index'], content['Item'], content['Cost'], content['price'], content['Tax'], content['Total'])
+    cursor.execute(sql, val)
+    mysql.get_db().commit()
+    resp = Response(status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/taxables', methods=['POST'])
+def new_taxables():
+    cursor = mysql.get_db().cursor()
+    content = request.json
+    sql = "INSERT INTO taxables (product_name, product_description, made_in, price, color) VALUES (%s,%s,%s,%s,%s)"
     val = (content['product_name'], content['product_description'], content['made_in'], content['price'], content['color'])
     cursor.execute(sql, val)
     mysql.get_db().commit()
@@ -110,22 +122,10 @@ def change_inventory(inventory_id):
     return resp
 
 
-@app.route('/api/v1/inventory', methods=['POST'])
-def new_inventory():
+@app.route('/api/v1/taxables/<int:taxables_id>', methods=['DELETE'])
+def remove_taxables(taxables_id):
     cursor = mysql.get_db().cursor()
-    content = request.json
-    sql = "INSERT INTO Inventory (product_name, product_description, made_in, price, color) VALUES (%s,%s,%s,%s,%s)"
-    val = (content['product_name'], content['product_description'], content['made_in'], content['price'], content['color'])
-    cursor.execute(sql, val)
-    mysql.get_db().commit()
-    resp = Response(status=200, mimetype='application/json')
-    return resp
-
-
-@app.route('/api/v1/inventory/<int:inventory_id>', methods=['DELETE'])
-def remove_inventory(inventory_id):
-    cursor = mysql.get_db().cursor()
-    sql = "DELETE FROM Inventory WHERE id = %s"
+    sql = "DELETE FROM taxables WHERE id = %s"
     val = (inventory_id,)
     cursor.execute(sql, val)
     mysql.get_db().commit()
--- db/init.sql
+++ db/init.sql
@@ -1,19 +1,22 @@
-CREATE DATABASE InventoryData;
-use InventoryData;
+CREATE DATABASE taxablesData;
+use taxablesData;
 
-CREATE TABLE IF NOT EXISTS Inventory (
-    `id` int AUTO_INCREMENT,
-    `product_name` VARCHAR(32) CHARACTER SET utf8,
-    `product_description` VARCHAR(128) CHARACTER SET utf8,
-    `made_in` VARCHAR(32) CHARACTER SET utf8,
-    `price` VARCHAR(8) CHARACTER SET utf8,
-    `color` VARCHAR(32) CHARACTER SET utf8,
-    PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS taxables (
+    `Index` INT,
+    `Item` VARCHAR(34) CHARACTER SET utf8,
+    `Cost` VARCHAR(24) CHARACTER SET utf8,
+    `Tax` NUMERIC(4, 2),
+    `Total` NUMERIC(4, 2),
+    `Column_6` NUMERIC(4, 2)
 );
-INSERT INTO Inventory(product_name, product_description, made_in, price, color) VALUES
-    ('Nightstand', 'fair condition, night stand built by prominent artist', 'New England', '125', 'white'),
-    ('Coffee table', 'good condition, coffee table perfect for living room', 'China', '25', 'brown'),
-    ('Bed', 'great condition, bueen sized bed', 'Brazil', '300', 'brown'),
-    ('lever', 'ok condition but think about all the levers you''ved pulled', 'Unknown', '10', 'black'),
-    ('kitchen table', 'excellent condition, can sit 4-6 people', 'New Jersey', '100', 'yellow'),
-    ('recliner', 'new, adjustable head rest and massage chair', 'Japan', '399', 'green')
+INSERT INTO taxables VALUES
+    ( 1,' "Fruit of the Loom Girl''s Socks"','  7.97', 0.60,  8.57,NULL),
+    ( 2,' "Rawlings Little League Baseball"',' 2.97', 0.22,  3.19,NULL),
+    ( 3,' "Secret Antiperspirant"','           1.29', 0.10,  1.39,NULL),
+    ( 4,' "Deadpool DVD"','                   14.96', 1.12, 16.08,NULL),
+    ( 5,' "Maxwell House Coffee 28 oz"','      7.28', 0.55,  7.83,NULL),
+    ( 6,' "Banana Boat Sunscreen',' 8 oz"',     6.68, 0.50,  7.18),
+    ( 7,' "Wrench Set',' 18 pieces"',          10.00, 0.75, 10.75),
+    ( 8,' "M and M',' 42 oz"',                  8.98, 0.67,  9.65),
+    ( 9,' "Bertoli Alfredo Sauce"','           2.12', 0.16,  2.28,NULL),
+    (10,' "Large Paperclips',' 10 boxes"',      6.19, 0.46,  6.65);
